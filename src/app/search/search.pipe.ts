4769a05e1f0cee249d7e6b746812ca0a95648aad
fasolka <korchanola@gmail.com>
2017-03-28 02:13:54 +0300

    implemented categories search

--- src/app/search/search.pipe.ts
+++ src/app/search/search.pipe.ts
@@ -4,11 +4,11 @@ import { Pipe, PipeTransform } from '@angular/core';
 
 export class SearchPipe implements PipeTransform {
   transform(value: any, exponent: string) {
-    console.log(value, "-", exponent);
-    return [{
-        amountOfItems: 3,
-        name: 'Music'
-    }];
+      console.log(value, "-", exponent);
+    return value.filter(item => {
+        console.log(item.name.toUpperCase(), "-", item.name.toUpperCase().indexOf(exponent.toUpperCase()));
+        return item.name.toUpperCase().indexOf(exponent.toUpperCase()) !== -1
+    })
   }
 }
 
