a751f05348b64f6fea484b592e5bb919f486c2d3
OleksanderPol <oleksander.polyarush@gmail.com>
2017-03-27 19:33:51 +0300

    commit before merging with dev. added item model, sorting service and small changes in template.

---
+++ src/app/item.model.ts
@@ -0,0 +1,5 @@
+export interface Item {
+  category: string;
+  owner: string;
+  items: Object[];
+}
---
+++ src/app/services/sorting.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+import { Item } from '../item.model';
+
+
+@Injectable()
+export class SoringService {
+  getItem(): void {}
+  // getSortedByDate(): Item[] {
+
+  // }
+}
