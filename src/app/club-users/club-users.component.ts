3f1c69b5dba8bc5db21704a0991aa3cf2007a020
fasolka <korchanola@gmail.com>
2017-04-01 21:35:10 +0300

    added club-users component

--- src/app/club-users/club-users.component.ts
+++ src/app/club-users/club-users.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { RequestService } from '../services/request.service';
 import { DataService } from '../services/data.service';
 import { User } from '../app.model';
+import { Router } from '@angular/router';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-club-users',
@@ -12,10 +14,15 @@ export class ClubUsersComponent implements OnInit {
   private users: User[];
 
   constructor(private requestService: RequestService,
-              private dataService: DataService) { }
+              private dataService: DataService,
+              private router: Router,
+              private sanitizer: DomSanitizer) { }
 
   ngOnInit() {
     this.users = this.dataService.getClubUsers();
     console.log(this.users);
+  }
+  showUser(username){
+    this.router.navigate(["home", username]);
   }
 }
